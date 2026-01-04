61c025e0604c35ee04bf2f82938499b1fde7823c
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-03 21:02:27 -0500

    update navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <div className="m-4 pb-4 border-b-2">
+    <div className="m-6 space-y-4">
       <p>val</p>
+      <div className="w-full h-[0.7] bg-white" />
     </div>
   );
 }
