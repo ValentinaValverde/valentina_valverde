7023ae2df2ba249ffe575f6c09639ee5deda8852
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-03 20:58:18 -0500

    header

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="p-6">
+      <p className="text-4xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+    </div>
+  );
+}
