36964e65513a9adf04ad1517dc56614535ebd4b2
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-04 20:41:19 -0500

    updated header text

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
     <div className="p-6 h-[75vh]">
-      <p className="text-4xl">Where technology meets creativity.</p>
+      <p className="text-4xl">
+        Hey, I&apos;m Val! I&apos;m a Design Engineer with 2+ years in
+        experience building SaaS products and various other creative projects.
+        With my love for art and eye for design, I am able to fill in the gaps
+        between developers and designers. I believe communication is key in
+        every relationship, and I absolutely love a good sandwich.
+      </p>
     </div>
   );
 }
