569d7bde19df03529c856ccd44ce5d5f4e259b4a
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-05 09:46:34 -0500

    implement custom font

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -37,7 +37,7 @@ export default function Contact() {
       </div>
 
       <div className="w-full p-6 h-[75vh] flex items-end justify-center">
-        <p className="text-[clamp(2rem,8vw,8rem)] p-6 opacity-25 hover:opacity-100 transition duration-300">
+        <p className="!font-mono text-[clamp(2rem,8vw,8rem)] p-6 opacity-25 hover:opacity-100 transition duration-300">
           sofficestudio@gmail.com
         </p>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="p-6 h-[75vh]">
-      <p className="text-4xl">
+      <p className="text-4xl !font-mono">
         Hey, I&apos;m Val! I&apos;m a Design Engineer with 2+ years in
         experience building SaaS products and various other creative projects.
         With my love for art and eye for design, I am able to fill in the gaps
